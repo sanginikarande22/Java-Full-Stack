d3fdca96cc590dc3d2fee758eca9f3c668c3f62b
Sangini Basavraj Karande <sanginikarande@gmail.com>
2025-11-02 12:10:58 +0530

    Add files via upload

---
+++ DataType_Day2.java
@@ -0,0 +1,14 @@
+
+public class DataType_Day2 {
+public static void main(String args[])
+{
+	System.out.println("byte range is " + Byte.MIN_VALUE +" to "+ Byte.MAX_VALUE + " bits");
+	System.out.println("short range is " + Short.MIN_VALUE +" to "+ Short.MAX_VALUE + " bits");
+	System.out.println("int range is " + Integer.MIN_VALUE +" to "+ Integer.MAX_VALUE + " bits");
+	System.out.println("long range is " + Long.MIN_VALUE +" to "+ Long.MAX_VALUE + " bits");
+	System.out.println("float range is " + Float.MIN_VALUE +" to "+ Float.MAX_VALUE + " bits");
+	System.out.println("double range is " + Double.MIN_VALUE +" to "+ Double.MAX_VALUE + " bits");
+	System.out.println("char range is " + Character.MIN_VALUE +" to "+ Character.MAX_VALUE + " bits");
+	System.out.println("Boolean range not found");
+}
+}
