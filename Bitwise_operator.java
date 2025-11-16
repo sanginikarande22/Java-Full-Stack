7ef57c7a65ddad5f3c85645db975f89eb84cff6c
Sangini Basavraj Karande <sanginikarande@gmail.com>
2025-11-16 16:11:09 +0530

    Add files via upload

---
+++ Bitwise_operator.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Bitwise_operator {
+	public static void main(String[] args) {
+	
+		System.out.println((12 & 15));
+		System.out.println(11| 13 & 12);
+		System.out.println(17 & 7 & 49 );
+		System.out.println(2 ^ 4);
+	}
+
+}
